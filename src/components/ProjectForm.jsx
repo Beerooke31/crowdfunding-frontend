65f0ddfc0cdd8dd8b5fd8ed0dc51cb104a1f1b58
Your Name <you@example.com>
2024-12-07 12:47:26 +0800

    Added create project functionality

--- src/components/ProjectForm.jsx
+++ src/components/ProjectForm.jsx
@@ -1,9 +1,54 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
+import postProject from "../api/post-project.js";
 import { useAuth } from "../hooks/use-auth.js";
 
 function ProjectForm() {
+  const navigate = useNavigate();
+  const { auth, setAuth } = useAuth();
+  const [projectDetail, setProjectDetail] = useState({
+    title: "",
+    description: "",
+    goal: "",
+    image: "",
+    is_open: true,
+    date_created: new Date().toISOString(),
+    owner: "",
+  });
+  const handleChange = (event) => {
+    const { id, value } = event.target;
+    setProjectDetail((prevProjectDetail) => ({
+      ...prevProjectDetail,
+      [id]: value,
+    }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (
+      projectDetail.title &&
+      projectDetail.description &&
+      projectDetail.goal &&
+      projectDetail.image
+    ) {
+      postProject(
+        projectDetail.title,
+        projectDetail.description,
+        projectDetail.goal,
+        projectDetail.image,
+        projectDetail.is_open,
+        projectDetail.date_created,
+        projectDetail.owner
+      ).then((response) => {
+        window.localStorage.setItem("token", response.token);
+        setAuth({
+          token: response.token,
+        });
+        navigate("/");
+      });
+    }
+  };
   return (
     <form>
       <div>
@@ -12,18 +57,39 @@ function ProjectForm() {
           type="text"
           id="title"
           placeholder="What's the title of your project?"
+          onChange={handleChange}
         />
       </div>
       <div>
         <label htmlFor="description">Description:</label>
-        <input type="description" id="description" placeholder="Description" />
+        <input
+          type="description"
+          id="description"
+          placeholder="Description"
+          onChange={handleChange}
+        />
       </div>
       <div>
-        <label htmlFor="Goal">Goal:</label>
-        <input type="goal" id="goal" placeholder="Fundraising goal" />
+        <label htmlFor="goal">Goal:</label>
+        <input
+          type="goal"
+          id="goal"
+          placeholder="Fundraising goal"
+          onChange={handleChange}
+        />
       </div>
-      {/* Figure out what to do with image */}
-      <button type="submit">Submit</button>
+      <div>
+        <label htmlFor="image">Image:</label>
+        <input
+          type="input"
+          id="image"
+          placeholder="Upload an image"
+          onChange={handleChange}
+        />
+      </div>
+      <button type="submit" onClick={handleSubmit}>
+        Submit
+      </button>
     </form>
   );
 }
